c3b7a0f3139eaf9ab3232b4b475200870ccd651f
Fhrzl A. <121169138+Rhesheyz@users.noreply.github.com>
2024-07-26 14:16:20 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
-class HalHeadset extends StatelessWidget {
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      child: new Center(
-        child: new Column(
-          children: <Widget>[
-            new Padding(padding: new EdgeInsets.all(20.0),),
-            new Text("HEADSET", style: new TextStyle(fontSize: 30.0),),
-            new Padding(padding: new EdgeInsets.all(20.0),),
-            new Icon(Icons.headset, size: 90.0,)
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Material App Bar'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Text('Hello World'),
       ),
     );
   }
